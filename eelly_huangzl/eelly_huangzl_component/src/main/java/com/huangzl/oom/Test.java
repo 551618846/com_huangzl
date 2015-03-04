759efd0e55652caac98323a95913d1c809609525
551618846 <551618846@qq.com>
2015-03-04 10:09:45 +0800

    oom分析

--- eelly_huangzl/eelly_huangzl_component/src/main/java/com/huangzl/oom/Test.java
+++ eelly_huangzl/eelly_huangzl_component/src/main/java/com/huangzl/oom/Test.java
@@ -29,9 +29,12 @@ public class Test {
      * 2,运行配置加vm参数:-Xms6M -Xmx12M -XX:PermSize=6m -XX:MaxPermSize=12m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=D:\
      * 3,运行,生成dump
      * 4,打开perspective:memory analysis:file>open heap dump生成分析
-     * 5,打开dominator tree,按retained heap(占用内存,gc该对象时会回收的内存)排序,查看哪些对象大量占用内存;
+     * 5,打开dominator tree,按retained heap(占用内存,gc该对象时会回收的内存)排序,查看哪些对象大量占用内存,或者存在大量对象;右键对象>List objects > with outgoning references查看本对象引用哪些对象
+     * 6,右键该对象>Path To GC Roots,查看被哪里引用,分析代码是否未释放内存
+     * 
      * shallow heap(浅堆,对象本身占用内存,不包含引用对象占用的内存)
      * retained heap(保留堆,包含引用对象占用的内存,可以理解为,gc该对象时会回收的内存)
+     * http://blog.csdn.net/angerlie/article/details/8039332
      */
     public static void test(){//
         List<String> s = new ArrayList<String>();
