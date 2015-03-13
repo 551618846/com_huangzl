f31669a220944241a1a70af054006af66ddf12f5
551618846 <551618846@qq.com>
2015-03-13 17:50:24 +0800

    场景设计

---
+++ eelly_huangzl/eelly_huangzl_component/src/main/java/com/huangzl/design/miaosha/Test.java
@@ -0,0 +1,28 @@
+/** 
+* @组件名：eelly_huangzl_component
+* @包名：com.huangzl.design.miaosha
+* @文件名：Test.java
+* @创建时间： 2015年3月13日 下午5:36:59
+* @版权信息：Copyright © 2014 eelly Co.Ltd,衣联网版权所有。
+*/
+
+package com.huangzl.design.miaosha;
+
+/**
+ * 
+ * 1,应用独立部署,数据预先加载到缓存
+ * 2,应用操作缓存,最终在持久化到数据库
+ * 3,
+ */
+public class Test {
+
+    /**
+     *
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
+
