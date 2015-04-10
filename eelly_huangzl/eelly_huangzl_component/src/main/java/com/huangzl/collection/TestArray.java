ce92919a97c7eb5c13391911c0015899e2b48795
551618846 <551618846@qq.com>
2015-04-10 10:04:47 +0800

    array也是Object

---
+++ eelly_huangzl/eelly_huangzl_component/src/main/java/com/huangzl/collection/TestArray.java
@@ -0,0 +1,11 @@
+package com.huangzl.collection;
+
+public class TestArray {
+
+	public static void main(String[] args) {
+		Integer[] z = new Integer[2];
+		
+		System.err.println(z.getClass());//数组也是Object
+	}
+
+}
