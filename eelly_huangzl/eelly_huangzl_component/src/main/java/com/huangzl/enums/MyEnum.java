ce7fd1406a7da001c94beb6fe3071632998cb3c1
551618846 <551618846@qq.com>
2015-04-10 10:35:19 +0800

    enum例子

--- eelly_huangzl/eelly_huangzl_component/src/main/java/com/huangzl/enums/MyEnum.java
+++ eelly_huangzl/eelly_huangzl_component/src/main/java/com/huangzl/enums/MyEnum.java
@@ -1,34 +1,74 @@
 package com.huangzl.enums;
 
-public enum MyEnum {
-	A,B,c(),D(4,"my name is D");//调用构造方法的2种方式
+
+public class MyEnum{
 	
-	private MyEnum(int index,String name) {//构造方法
-		this.index = index;
-		this.name = name;
+	public static void main(String[] args) {
+		System.err.println(MyEnumz.A);
+		
+		MyEnumz.A.setName("xx");
+		System.err.println(MyEnumz.A.getName());
+		
+		
+		System.err.println(MyEnumz.D.getName());
+		MyEnumz.D.print();
+
 	}
 	
-	private MyEnum() {//构造方法
-		// TODO Auto-generated constructor stub
+	interface MyInter{
+		void print();
 	}
 	
+	/**
+	 * 所有的枚举都继承自java.lang.Enum类。由于Java 不支持多继承，所以枚举对象不能再继承其他类。
+	 * 可以实现接口
+	 * @author Administrator
+	 *
+	 */
+	enum MyEnumz implements MyInter{//不能继承,只能实现接口
+		A,B,c(),D(4,"my name is D");//调用构造方法的2种方式
+		
+		private MyEnumz(int index,String name) {//构造方法
+			this.index = index;
+			this.name = name;
+		}
+		
+		private MyEnumz() {//构造方法
+			
+		}
+		
+		
+		private int index;
+		private String name;
+		
+		
+		public int getIndex() {
+			return index;
+		}
+		public void setIndex(int index) {
+			this.index = index;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		@Override
+		public void print() {
+			System.err.println(this.index + "-" + this.name);
+			
+		}
+		
+		
+	}
 	
-	private int index;
-	private String name;
-	
-	
-	public int getIndex() {
-		return index;
-	}
-	public void setIndex(int index) {
-		this.index = index;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
 	
 	
 }
+
+
+
+
+
