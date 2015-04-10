d874dfdb03d32affcbabbf1442c6474adf325018
551618846 <551618846@qq.com>
2015-04-10 10:54:52 +0800

    enum例子

--- eelly_huangzl/eelly_huangzl_component/src/main/java/com/huangzl/enums/MyEnum.java
+++ eelly_huangzl/eelly_huangzl_component/src/main/java/com/huangzl/enums/MyEnum.java
@@ -1,5 +1,9 @@
 package com.huangzl.enums;
 
+import java.util.EnumMap;
+import java.util.EnumSet;
+
+
 
 public class MyEnum{
 	
@@ -12,63 +16,81 @@ public class MyEnum{
 		
 		System.err.println(MyEnumz.D.getName());
 		MyEnumz.D.print();
-
+		
+		
+		EnumSet<MyEnumz> sets = EnumSet.allOf(MyEnumz.class);//EnumSet
+		for(MyEnumz t:sets){
+			t.print();
+		}
+		
+		EnumMap<MyEnumz, String> map = new EnumMap<MyEnumz, String>(MyEnumz.class);//EnumMap
+		
+		
+		System.err.println(MyEnumz.valueOf("D"));
+		
+		
+		MyEnumz[] list = MyEnumz.values();
+		for(MyEnumz t : list){
+			t.print();
+		}
+		
+		
 	}
 	
-	interface MyInter{
-		void print();
-	}
 	
-	/**
-	 * 所有的枚举都继承自java.lang.Enum类。由于Java 不支持多继承，所以枚举对象不能再继承其他类。
-	 * 可以实现接口
-	 * @author Administrator
-	 *
-	 */
-	enum MyEnumz implements MyInter{//不能继承,只能实现接口
-		A,B,c(),D(4,"my name is D");//调用构造方法的2种方式
-		
-		private MyEnumz(int index,String name) {//构造方法
-			this.index = index;
-			this.name = name;
-		}
-		
-		private MyEnumz() {//构造方法
-			
-		}
-		
-		
-		private int index;
-		private String name;
-		
-		
-		public int getIndex() {
-			return index;
-		}
-		public void setIndex(int index) {
-			this.index = index;
-		}
-		public String getName() {
-			return name;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		@Override
-		public void print() {
-			System.err.println(this.index + "-" + this.name);
-			
-		}
-		
-		
-	}
 	
 	
 	
 }
 
+interface MyInter{
+	void print();
+}
 
+/**
+ * 所有的枚举都继承自java.lang.Enum类。由于Java 不支持多继承，所以枚举对象不能再继承其他类。
+ * 可以实现接口
+ * @author Administrator
+ *
+ */
+enum MyEnumz implements MyInter{//不能继承,只能实现接口
+	A,B,c(),D(4,"my name is D");//调用构造方法的2种方式
+	
+	private MyEnumz(int index,String name) {//构造方法
+		this.index = index;
+		this.name = name;
+	}
+	
+	private MyEnumz() {//构造方法
+		
+	}
+	
+	
+	private int index;
+	private String name;
+	
+	
+	public int getIndex() {
+		return index;
+	}
+	public void setIndex(int index) {
+		this.index = index;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public void print() {
+		System.err.println(this.index + "-" + this.name);
+		
+	}
+	
+	
+}
 
 
 
